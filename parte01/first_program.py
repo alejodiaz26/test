6e17331dbcfea34b6d496ec12c2da5102275c788
alejodiaz26 <58312576+alejodiaz26@users.noreply.github.com>
2023-10-20 05:23:52 -0500

    First Program In Python

---
+++ parte01/first_program.py
@@ -0,0 +1,11 @@
+#### Create a function in python that sum two numbers.
+
+def suma(a,b):
+    return a + b
+
+first_number = int(input("Insert the first Number: "))
+second_number = int(input("Insert the second number: "))
+
+result = suma(first_number, second_number)
+
+print(f"The result of the addition is equal to: {result}")
